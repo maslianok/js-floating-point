7f1ca6b821eb1dff0999aaca41f02fbf8c6a733d
maslianok <maslianok@gmail.com>
2019-11-19 22:04:39 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * your code goes here
+ * */
+
+export default function myModule(a, b) {
+  return a + b;
+}
---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+/**
+ * your tests goes here
+ * */
+
+import myModule from '../src/index';
+
+test('your module', () => {
+  expect(myModule(1, 2)).toBe(3);
+});
